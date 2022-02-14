dd8374ec34225f81c98706b35733c6569d1cd5a9
Ivan Gabriele <ivan.gabriele@gmail.com>
2022-02-14 21:33:26 +0100

    fix: make commit message lowercase

--- make.js
+++ make.js
@@ -6,8 +6,8 @@ const [type] = process.argv.slice(2)
 const now = new Date().toUTCString()
 await fs.writeFile(`./dummies/${type}.txt`, now, 'utf-8')
 
-const commitMessage = `${type}: ${now}`
-const branchName = commitMessage.toLowerCase().replace(/[^a-z0-9]+/g, '-')
+const commitMessage = `${type}: ${now}`.toLowerCase()
+const branchName = commitMessage.replace(/[^a-z0-9]+/g, '-')
 
 await execa('git', ['checkout', '-B', branchName])
 await execa('git', ['add', '.'])
